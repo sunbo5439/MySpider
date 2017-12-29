399751e67eda8ed9e85519699bcbecb2fc42a9f2
sunbo <sunbo5439@qq.com>
2017-12-29 10:50:18 +0800

    new

--- settings.py
+++ settings.py
@@ -30,7 +30,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+#DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -91,3 +91,13 @@ COOKIES_ENABLED = False
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+PROXIES = [
+    {'ip_port': '218.56.132.157:8080', 'user_pass': ''},
+    {'ip_port': '27.46.43.107:9797', 'user_pass': ''},
+    {'ip_port': '218.20.54.77:9999', 'user_pass': ''},
+    {'ip_port': '101.6.33.224:8123', 'user_pass': ''},
+    {'ip_port': '112.126.91.129:3128', 'user_pass': ''},
+    {'ip_port': '101.201.115.184:8088', 'user_pass': ''},
+]
